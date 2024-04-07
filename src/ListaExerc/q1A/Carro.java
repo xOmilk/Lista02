d7f5d6ae49388356d83f31ff2d7a89fa42f85f1b
xOmilk <leite46052@gmail.com>
2024-04-06 22:03:03 -0300

    Carro

--- src/ListaExerc/q1A/Carro.java
+++ src/ListaExerc/q1A/Carro.java
@@ -6,6 +6,7 @@ public class Carro {
     String cor;
     String modelo;
     int anoFab;
+    String tipoCombustivel;
     int numPortas;
     int qntdDisponivel;
     boolean isBasic;
@@ -14,6 +15,7 @@ public class Carro {
         System.out.println("Cor: "+this.cor);
         System.out.println("Marca: "+this.marca);
         System.out.println("Código: "+this.codigo);
+        System.out.println("Tipo de combustível: "+this.tipoCombustivel);
         System.out.println("Ano Fabricação: "+this.anoFab);
         System.out.println("num Portas: "+this.numPortas);
         System.out.println("Quantidade disponível: "+this.qntdDisponivel);
@@ -21,7 +23,7 @@ public class Carro {
 
     }
 
-    public void cadastrar(int codigo, String marca, String cor, String modelo, int anoFab, int numPortas, int qntdDisponivel, boolean isBasic) {
+    public void cadastrar(int codigo, String marca, String cor, String modelo, int anoFab, int numPortas, int qntdDisponivel,String tipoCombustivel ,boolean isBasic) {
         this.codigo = codigo;
         this.marca = marca;
         this.cor = cor;
@@ -29,10 +31,11 @@ public class Carro {
         this.anoFab = anoFab;
         this.numPortas = numPortas;
         this.qntdDisponivel = qntdDisponivel;
+        this.tipoCombustivel=tipoCombustivel;
         this.isBasic = isBasic;
         System.out.println("CADASTRO CONCLUIDO");
     }
-    public void editar(int codigo, String marca, String cor, String modelo, int anoFab, int numPortas, int qntdDisponivel, boolean isBasic) {
+    public void editar(int codigo, String marca, String cor, String modelo, int anoFab, int numPortas, int qntdDisponivel,String tipoCombustivel ,boolean isBasic) {
         this.codigo = codigo;
         this.marca = marca;
         this.cor = cor;
@@ -40,6 +43,7 @@ public class Carro {
         this.anoFab = anoFab;
         this.numPortas = numPortas;
         this.qntdDisponivel = qntdDisponivel;
+        this.tipoCombustivel=tipoCombustivel;
         this.isBasic = isBasic;
         System.out.println("EDIÇÃO CONCLUIDA");
     }
--- src/ListaExerc/q1A/ProgramaCarro.java
+++ src/ListaExerc/q1A/ProgramaCarro.java
@@ -5,16 +5,16 @@ public class ProgramaCarro {
         Carro c1= new Carro();
         Carro c2= new Carro();
 
-        c1.cadastrar(456,"Ford","Amarelo","Modelo esquisito",2000,4,10,false);
-        c2.cadastrar(657,"Geforce","Preto","Do bom",2024,4,5,false);
+        c1.cadastrar(456,"Ford","Amarelo","Modelo esquisito",2000,4,10,"Alcool",false);
+        c2.cadastrar(657,"Geforce","Preto","Do bom",2024,4,5,"Alcool",false);
 
         System.out.println("Primeiro carro antes da alteracao");
         c1.printStatus();
         System.out.println("\n\nSegundo carro antes da alteracao");
         c2.printStatus();
 
-        c1.editar(555,"AAAAAAAAAAA","Amarelo FOSCO","Modelo menos esquisito", 2000, 4, 9,false);
-        c2.editar(456,"Ford","Amarelo","Modelo esquisito",2000,4,10,false);
+        c1.editar(555,"AAAAAAAAAAA","Amarelo FOSCO","Modelo menos esquisito", 2000, 4, 9,"Gasolina",false);
+        c2.editar(456,"Ford","Amarelo","Modelo esquisito",2000,4,10,"Diesel",false);
 
         System.out.println("\nPassei as caracteristicas originais do primeiro carro para as caracteristicas editadas do segundo carro\n");
 
